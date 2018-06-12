7367c192fb2ec236db13e409bae96a9da95a8d33
Vladimir <Konjkov.VV@gmail.com>
2018-06-12 23:00:50 +0700

    in progress

--- molden2qmc.py
+++ molden2qmc.py
@@ -783,55 +783,64 @@ ORBITAL COEFFICIENTS (normalized AO)
     def multiple_fits(self):
         """Fit all orbitals in GTO basis"""
 
-        def slater_1(x, a1, zeta1, a2, zeta2):
+        def slater_1(r, a1, zeta1):
             """Best fit for n=0 orbital from ano-pVDZ set for He-Ne"""
-            return a1*np.exp(-zeta1*x) + a2*np.exp(-zeta2*x)
+            return a1*np.exp(-zeta1*r)
 
-        def slater_2(x, a1, b1, zeta1, a2, zeta2):
-            """Best fit for n=1 orbital from ano-pVDZ set for He-Ne"""
-            return (a1+b1*x)*np.exp(-zeta1*x) + a2*np.exp(-zeta2*x)
+        def slater_2(r, a1, zeta1, a2, zeta2):
+            """Best fit for n=1 orbital from ano-pVDZ set for Be-Ne"""
+            return a1*np.exp(-zeta1*r) + a2*r*np.exp(-zeta2*r)
 
-        def slater_3(x, a1, b1, c1, zeta1):
+        def slater_3(r, a1, zeta1, a2, zeta2):
             """Best fit for n=2 orbital from ano-pVDZ set for Li-Ne"""
-            return (a1+b1*x+c1*x*x)*np.exp(-zeta1*x)
+            return a1*np.exp(-zeta1*r) + a2*r**2*np.exp(-zeta2*r)
 
-        fit = [slater_1, slater_2, slater_3]
+        fit_function = [slater_1, slater_2, slater_3]
+        p0 = [(1, 1), (1, 1, 1, 1), (1, 1, -1, 1)]
 
         n = 0
         prev_shell_type = ''
-        for shell in self.atom_list[0]['SHELLS']:
-            if shell['TYPE'] == prev_shell_type:
-                n += 1
-            else:
-                n = 0
-            xdata = np.linspace(0.0, 9.0, 50)
-            ydata = self.gaussian(xdata, shell['DATA'])
-            self.fit(fit[n], xdata, ydata)
+        for atom in self.atom_list:
+            for shell in atom['SHELLS']:
+                if shell['TYPE'] == prev_shell_type:
+                    n += 1
+                else:
+                    prev_shell_type = shell['TYPE']
+                    n = self.ang_momentum_map[shell['TYPE']]
+                xdata = np.linspace(0.0, 2.0, 50)
+                ydata = self.gaussian(xdata, shell['DATA'])
+                print('{}{} orbital'.format(n+1, shell['TYPE']))
+                print(p0[n])
+                popt, perr = self.fit(
+                    fit_function[n],
+                    xdata, ydata,
+                    p0[n]
+                )
+                print(popt, perr)
 
-    def fit(self, fit_function, xdata, ydata):
+    def fit(self, fit_function, xdata, ydata, p0):
         """Fit gaussian basis by slater"""
 
         plot = True
 
         try:
-            popt, pcov = curve_fit(fit_function, xdata, ydata)
+            popt, pcov = curve_fit(fit_function, xdata, ydata, p0)
             perr = np.sqrt(np.diag(pcov))
             print(popt, perr)
             if plot:
                 plt.plot(xdata, ydata, 'b-', label='data')
                 plt.plot(xdata, fit_function(xdata, *popt), 'r-', label='fit')
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.legend()
-                plt.show()
         except Exception as e:
             print(e)
+            popt, perr = [], []
             if plot:
                 plt.plot(xdata, ydata, 'b-', label='data')
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.legend()
-                plt.show()
+        if plot:
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.legend()
+            plt.show()
+        return popt, perr
 
 
 class DefaultConverter(STOWFN):
